551ff33bcc12d8730a467cde9e5ec8da28f6bacd
Dilawar Singh <dilawars@ncbs.res.in>
2018-04-06 10:10:56 +0530

    Python script to generate pdf.

---
+++ pandoc2pdf.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+"""pandoc2pdf.py: 
+
+"""
+    
+__author__           = "Dilawar Singh"
+__copyright__        = "Copyright 2017-, Dilawar Singh"
+__version__          = "1.0.0"
+__maintainer__       = "Dilawar Singh"
+__email__            = "dilawars@ncbs.res.in"
+__status__           = "Development"
+
+import sys
+import os
+import re
+from pandoc import filters
+import subprocess
+
+def main( args ):
+    pandocSrc = args.input
+    pdf = args.output
+    filters = filters.generic_filters( )
+    print( filters )
+    
+
+if __name__ == '__main__':
+    import argparse
+    # Argument parser.
+    description = '''pandoc2pdf.'''
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument('--input', '-i'
+        , required = True
+        , help = 'Input file (pandoc)'
+        )
+    parser.add_argument('--output', '-o'
+        , required = False
+        , help = 'Output file'
+        )
+    parser.add_argument( '--debug', '-d'
+        , required = False
+        , default = 0
+        , type = int
+        , help = 'Enable debug mode. Default 0, debug level'
+        )
+    class Args: pass 
+    args = Args()
+    parser.parse_args(namespace=args)
+    main( args )
+
